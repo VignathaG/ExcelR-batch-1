1119d831ffb0d1bf609e2dfe7bcfe6644f1d2892
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-01-04 12:19:53 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String args[]) {
+        String username = "Vignatha";
+        String pwd = "achhu@07";
+        while((username == "Vignatha") && (pwd == "achhu@07")) {
+            System.out.println("hello Achhu");
+            break;
+        }
+    }
+    
+}
