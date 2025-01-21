fdd9e4e54e83c26853aad7c66f519d7bfbfce633
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-01-21 11:39:49 +0530

    Area of Triangle

--- PS001/Demo012.java
+++ PS001/Demo012.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo02 {
+public class Demo012 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the base of the triangle: ");
