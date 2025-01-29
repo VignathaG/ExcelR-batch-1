6f4931cce7fb81964a9feacc7c79e11927a2040c
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-01-29 11:32:01 +0530

    Update LTCode008.java

--- Leetcode/LTCode008.java
+++ Leetcode/LTCode008.java
@@ -1,32 +1,45 @@
-public class LTCode008 {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1;
-        int p2 = n - 1;
-        int p = m + n - 1;
+import java.util.Arrays;
 
-        while ((p1 >= 0) && (p2 >= 0)) {
-            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+public class LTCode008 {
+    
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;  
+        int j = n - 1;  
+        int k = m + n - 1;  
+        
+       
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
         }
 
         
-        while (p2 >= 0) {
-            nums1[p--] = nums2[p2--];
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
+        
+       
     }
 
     public static void main(String[] args) {
-        LTCode008 solution = new LTCode008();
         int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int m = 3;
         int[] nums2 = {2, 5, 6};
-        int n = 3;
+        int m = 3, n = 3;
+
+        merge(nums1, m, nums2, n);
+
         
-        solution.merge(nums1, m, nums2, n);
-        
-        System.out.print("Merged array: ");
-        for (int num : nums1) {
-            System.out.print(num + " ");
-        }
+        System.out.println(Arrays.toString(nums1));
     }
 }
+
+
 
