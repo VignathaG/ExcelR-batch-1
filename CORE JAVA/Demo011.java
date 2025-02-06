d572f6dcaa054dcfbc02892854ee443845eea68d
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-02-06 11:35:08 +0530

    Inheritance

--- CORE JAVA/Demo011.java
+++ CORE JAVA/Demo011.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Demo01 {
+public class Demo011 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter radius:");
