c08214718a29cbb7c0eb8a69814278ec888550b2
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-02-06 11:40:57 +0530

    Adding 2 Digits

--- CORE JAVA/Demo017.java
+++ CORE JAVA/Demo017.java
@@ -12,7 +12,7 @@ public class Demo017 {
     public static void main(String[] args) {
         int n = 78;
         int sum = SumOf2Digits.getSumOf2Digits(n); 
-        System.out.println("The sum of 2 digits is: " + sum);
+        System.out.println("The sum of 2 digits is: " +  sum);
     }
 }
 
