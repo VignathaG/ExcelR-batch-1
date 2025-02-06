068d0d9712967faf69419e121855b61bf5827e12
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-02-06 11:21:15 +0530

    Do While User Details

--- CORE JAVA/Demo008.java
+++ CORE JAVA/Demo008.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo00 {
+public class Demo008 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String username,pass;
