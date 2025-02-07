5140fd4d470d108b5cb4b18bae1368059b7b4554
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-02-07 11:04:43 +0530

    Reverse String

--- CORE JAVA/Demo025.java
+++ CORE JAVA/Demo025.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class reverseString {
     void revStr(String str){
-        char[] ch = str.toCharArray();
+        char[] ch = str.toCharArray( );
         for(int i=ch.length-1;i>=0;i--){
             System.out.print(ch[i]);
         }
