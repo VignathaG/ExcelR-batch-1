fc901c80291f2f87a71ce46c240267f0e54c7755
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-02-07 11:18:50 +0530

    Even or Odd checker

--- CORE JAVA/Demo034.java
+++ CORE JAVA/Demo034.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class Demo034 {
-    public static void main(String[] args) {
+    public static void main(String[ ] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
