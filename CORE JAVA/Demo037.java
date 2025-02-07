56cc21fc9fa08b5d53335094e368042ca352fe94
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-02-07 11:27:45 +0530

    Bigger no among four

--- CORE JAVA/Demo037.java
+++ CORE JAVA/Demo037.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class Demo037 {
-    public static void main(String[] args) {
+    public static void main(String[ ] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter first number: ");
             int num1 = scanner.nextInt();
