deb4036aac70ca6bbdfd96a511bc4b0cd54f7641
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-02-07 11:40:42 +0530

    character lowercase or uppercase or digit

--- CORE JAVA/Demo039.java
+++ CORE JAVA/Demo039.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class Demo039 {
-    public static void main(String[] args) {
+    public static void main(String[ ] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Enter a character:");
             char character = scanner.next().charAt(0);
