b90037dad5f8f89701af57103ed97fe31d8829de
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-02-11 11:34:11 +0530

    uppercase to lowercase

--- CORE JAVA/Demo043.java
+++ CORE JAVA/Demo043.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class Demo043 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter an uppercase character:");
