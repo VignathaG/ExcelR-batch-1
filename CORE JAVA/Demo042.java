b30482dd66b85b74a19d9e8a5c571dc908da2e1b
VignathaG <2311cs010238@mallareddyuniversity.ac.in>
2025-02-11 11:32:45 +0530

    lowercase character into uppercase

--- CORE JAVA/Demo042.java
+++ CORE JAVA/Demo042.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class Demo042 {
-    public static void main(String[] args) {
+    public static void main(String[] args ) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a lowercase character:");
